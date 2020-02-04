c363c8bdf1e6598d2135969d171cae21d4761040
MasterOfMenace <nero_wolf@mail.ru>
2020-02-04 23:31:40 +0300

    поправил настройки линтера, создал модули App и Main

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app.jsx';
 
+const Settings = {
+  OFFER_COUNT: 1500,
+};
+
+ReactDOM.render(
+    <App
+      offerCount={Settings.OFFER_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
