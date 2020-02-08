12f239d579d2dc13fc65a139ca49f29bd05260c9
MasterOfMenace <nero_wolf@mail.ru>
2020-02-08 19:49:45 +0300

    Установил prop-types, добавил propTypes для Main и App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 
 const App = (props) => {
-  const {offerCount} = props; // eslint-disable-line react/prop-types
+  const {offerCount, placesName} = props;
   return (
-    <Main offerCount={offerCount}/>
+    <Main
+      offerCount={offerCount}
+      placesName={placesName}/>
   );
+};
+
+App.propTypes = {
+  offerCount: PropTypes.number.isRequired,
+  placesName: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import App from './components/app/app.jsx';
 
 const Settings = {
   OFFER_COUNT: 1500,
+  PLACES_NAME: [
+    `Beautiful & luxurious apartment at great location`,
+    `Wood and stone place`,
+    `Canal View Prinsengracht`,
+    `Nice, cozy, warm big bed apartment`,
+    `SandalWood and RollingStone place`
+  ],
 };
 
 ReactDOM.render(
     <App
       offerCount={Settings.OFFER_COUNT}
+      placesName={Settings.PLACES_NAME}
     />,
     document.querySelector(`#root`)
 );
