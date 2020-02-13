3257e4b6669b6a3df65b2285816f966577e43663
MasterOfMenace <nero_wolf@mail.ru>
2020-02-13 22:44:47 +0300

    Создал компоненты OfferCard (карточка предложения) и OffersList (список предлжений). Создал файл с моковыми данными по предложениям. Добавил состояние в компонент OffersList. Реализовал изменение состояния при наведении на карточку предложения. Рефакторил компонент Main для отрисовки компонента OffersList вместо статической верстки. Написал снепшот тест для компонента OfferCard.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,21 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Main from '../main/main.jsx';
 
-const titleClickHandler = () => {};
+// const titleClickHandler = () => {};
 
 const App = (props) => {
-  const {offerCount, placesName} = props;
+  const {offerCount, offers} = props;
   return (
     <Main
       offerCount={offerCount}
-      placesName={placesName}
-      titleClickHandler={titleClickHandler}/>
+      offers={offers}/>
   );
 };
 
 App.propTypes = {
   offerCount: PropTypes.number.isRequired,
-  placesName: PropTypes.arrayOf(PropTypes.string).isRequired
+  offers: PropTypes.array.isRequired
 };
 
 export default App;
---
+++ src/components/offer-card/offer-card.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import OfferCard from './offer-card.jsx';
+
+const mockOffer = {
+  name: `Room in hotel`,
+  picture: `img/apartment-01.jpg`,
+  price: 100,
+  type: `Private room`
+};
+
+it(`Правильное отображение компонента OfferCard`, () => {
+  const tree = renderer
+    .create(<OfferCard
+      offer={mockOffer}
+      onMouseOver={()=>{}}
+      onMouseLeave={()=>{}}/>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
