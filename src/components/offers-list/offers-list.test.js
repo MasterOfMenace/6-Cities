835cb0da41d13013e2079800a72dadaafa096b05
MasterOfMenace <nero_wolf@mail.ru>
2020-02-13 22:57:35 +0300

    Написал снепшот тест для компонента OffersList

---
+++ src/components/offers-list/offers-list.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import OffersList from './offers-list';
+
+const mockOffers = [
+  {
+    name: `Offer1`,
+    picture: `img/apartment-01.jpg`,
+    price: 100,
+    type: `Private room`
+  },
+  {
+    name: `Offer2`,
+    picture: `img/apartment-01.jpg`,
+    price: 200,
+    type: `Apartment`
+  },
+  {
+    name: `Offer3`,
+    picture: `img/apartment-01.jpg`,
+    price: 300,
+    type: `Hostel`
+  },
+  {
+    name: `Offer4`,
+    picture: `img/apartment-01.jpg`,
+    price: 400,
+    type: `Apartment`
+  },
+];
+
+it(`Правильное отображение компонента OffersList`, () => {
+  const tree = renderer
+    .create(<OffersList
+      offers={mockOffers}/>)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
