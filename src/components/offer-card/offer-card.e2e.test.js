5ba75e72c81ff87f7cbe5adcdbd53ef9f6d0781b
MasterOfMenace <nero_wolf@mail.ru>
2020-02-13 23:51:12 +0300

    Написал e2e тест для компонента OfferCard

---
+++ src/components/offer-card/offer-card.e2e.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import OfferCard from './offer-card.jsx';
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const mockOffer = {
+  name: `Room in hotel`,
+  picture: `img/apartment-01.jpg`,
+  price: 100,
+  type: `Private room`
+};
+
+
+it(`При наведении курсора на карточку предложения в обработчик попадает информация об объекте недвижимости`, () => {
+  const offer = mockOffer;
+  const onMouseOver = jest.fn();
+  const onMouseLeave = jest.fn();
+  const hoveredOffer = {
+    name: `Room in hotel`,
+    picture: `img/apartment-01.jpg`,
+    price: 100,
+    type: `Private room`
+  };
+
+  const offerCard = shallow(
+      <OfferCard
+        offer={offer}
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}/>
+  );
+
+  const card = offerCard.find(`.place-card`);
+  card.simulate(`mouseover`, {target: offer});
+
+  expect(onMouseOver).toHaveBeenCalledTimes(1);
+
+  expect(onMouseOver.mock.calls[0][0].target).toMatchObject(hoveredOffer);
+});
+
