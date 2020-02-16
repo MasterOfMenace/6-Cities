4980e0387ee4e3f36a7c745a78e2da98a545b31b
MasterOfMenace <nero_wolf@mail.ru>
2020-02-16 23:50:43 +0300

    Переписал компонент App на классы, настроил роутинг

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import Main from '../main/main.jsx';
+import OfferDetails from '../offer-details/offer-details.jsx';
 
-const App = (props) => {
-  const {offerCount, offers} = props;
-  return (
-    <Main
-      offerCount={offerCount}
-      offers={offers}
-      titleClickHandler={()=>{}}/>
-  );
-};
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {offerCount, offers} = this.props;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main
+              offerCount={offerCount}
+              offers={offers}
+              titleClickHandler={()=>{}}
+            />
+          </Route>
+          <Route exact path="/dev-details">
+            <OfferDetails
+              offer={offers[0]}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
 App.propTypes = {
   offerCount: PropTypes.number.isRequired,
