3fea661b464740030045d933485b44de8c7439a4
MasterOfMenace <nero_wolf@mail.ru>
2020-02-19 23:00:24 +0300

    Написал снепшот тесты для компонентов Map и OfferDetails

---
+++ src/components/map/map.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Map from './map.jsx';
+
+const offersLocations = [
+  [52.369553943508, 4.85309666406198],
+  [52.369553943508, 4.85309666406198],
+  [52.369553943508, 4.85309666406198],
+  [52.369553943508, 4.85309666406198]
+];
+
+it(`Првильное отображение компонента Map`, () => {
+  const tree = renderer
+    .create(<Map
+      offersLocations={offersLocations}/>,
+    {
+      createNodeMock: () => document.createElement(`div`)
+    })
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
